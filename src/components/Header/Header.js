582a6a0b92f80f0e95dbae4db2f195dde894c3a6
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-10 22:47:21 +0600

    Header section completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from "../../images/logo.png";
 
 const Header = () => {
     return (
-        <nav>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/quizes">Quizes</NavLink>
-            <NavLink to="/statistics">Statistics</NavLink>
-            <NavLink to="/blogs">Blogs</NavLink>
-            <NavLink to="/about">About</NavLink>
-        </nav>
+        <div className='bg-[#1e2b47] md:flex md:justify-between justify:center items-center text-center py-3 px-10'>
+            <div className='flex items-center gap-4'>
+                <img className='w-12 h-12 rounded-full' src={Logo} alt="logo" />
+                <h3 className='text-3xl text-white font-bold'>Achievment Hunter</h3>
+            </div>
+            <nav className='text-white flex gap-3'>
+                <NavLink to="/home" className="md:text-2xl text-xl">Home</NavLink>
+                <NavLink to="/quizes" className="md:text-2xl text-xl">Quizes</NavLink>
+                <NavLink to="/statistics" className="md:text-2xl text-xl">Statistics</NavLink>
+                <NavLink to="/blogs" className="md:text-2xl text-xl">Blogs</NavLink>
+                <NavLink to="/about" className="md:text-2xl text-xl">About</NavLink>
+            </nav>
+        </div>
     );
 };
 
