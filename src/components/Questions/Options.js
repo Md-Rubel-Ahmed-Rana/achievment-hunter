7e94187fb2f663cccbb87ca6b6d334065b573730
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-11 03:04:09 +0600

    Currect answer checked successfully

--- src/components/Questions/Options.js
+++ src/components/Questions/Options.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Options = ({option}) => {
-    console.log(option);
+const Options = ({ option, correctAnswer }) => {
+
+
+    const checkAnswer = (answer) => {
+        if (answer === correctAnswer){
+            toast("Your Answer is currect")
+        }else{
+            toast("Your Answer is not currect")
+        }
+    }
     return (
         <div>
-            <p className='bg-emerald-600 m-2 p-5 text-white rounded-lg'>
+            <p onClick={() => checkAnswer(option)} className='bg-emerald-600 m-2 p-5 text-white rounded-lg cursor-pointer'>
                 <small> <input type="radio" name="" id="" /> </small>
                 <small className='text-xl'>{option}</small>
             </p>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Options from './Options';
 
 const Questions = ({ singleQuestion }) => {
-    const { question , options}  = singleQuestion
+    const { question, options, correctAnswer }  = singleQuestion
+    console.log("Answer", correctAnswer);
     
     return (
         <div className='border my-2 p-4'>
             <h3 className='text-2xl px-2 text-white mb-5'><strong>Question:</strong> {question}</h3>
             <div className='grid grid-cols-2  mx-auto'>
                 {
-                    options.map((option) => <Options option={option} />)
+                    options.map((option, index) => <Options option={option} key={index} correctAnswer={correctAnswer} />)
                 }
             </div>
         </div>
