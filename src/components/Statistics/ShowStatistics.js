05b203dbd248d6711c92896ce3f609311c0a7ab8
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-12 01:39:40 +0600

    Style updated

--- src/components/Statistics/ShowStatistics.js
+++ src/components/Statistics/ShowStatistics.js
@@ -3,21 +3,22 @@ import { LineChart, Legend, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'r
 
 const ShowStatistics = ({name, total}) => {
     const data = [
-        { "name": `${name[0]}`, "Topics": `${total[0]}`},
-        { "name": `${name[1]}`, "Topics": `${total[1]}`},
-        { "name": `${name[2]}`, "Topics": `${total[2]}`},
-        { "name": `${name[3]}`, "Topics": `${total[3]}`}
+        { "name": `${name[0]}`, "Question": `${total[0]}`},
+        { "name": `${name[1]}`, "Question": `${total[1]}`},
+        { "name": `${name[2]}`, "Question": `${total[2]}`},
+        { "name": `${name[3]}`, "Question": `${total[3]}`}
     ]
+    
     return (
-        <div className='flex justify-center bg-gray-900 py-28 px-'>
+        <div className='bg-gray-900 px-40 py-28 flex justify-center'>
             <LineChart  width={400} height={250} data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" stroke="#ffffff" />
-                <YAxis stroke="#ffffff" />
+                <XAxis dataKey="name" />
+                <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="Topics" stroke="#ffffff" />
+                <Line type="monotone" dataKey="Question" stroke="#8884d8" />
             </LineChart>
         </div>
     );
