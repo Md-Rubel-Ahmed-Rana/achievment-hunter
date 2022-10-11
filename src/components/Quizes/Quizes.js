d158f2d30b23dc4bca8d07f85fcf3098da01b3bd
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-11 22:30:38 +0600

    Bugs fixed

--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -6,7 +6,7 @@ const Quizes = () => {
     const {data} = useLoaderData();
     return (
         <div className=' bg-black'>
-            <h3 className='text-white text-center text-4xl font-bold pt-4'>Choose Your Best Option</h3>
+            <h3 className='text-white text-center text-2xl md:text-4xl font-bold pt-4'>Choose Your Best Option</h3>
             <div className='md:flex justify-between gap-4 md:py-20 py-5 md:px-40 px-10'>
                 {
                     data.map((quiz) => <DisplayQuiz quiz={quiz} key={quiz.id} />)
