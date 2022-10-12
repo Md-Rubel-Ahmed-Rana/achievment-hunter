8edc0f421c57d664834a01a3b63d4882e9b7adbf
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-12 13:17:11 +0600

    Error Page completed

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h3>This is Error Page</h3>
-            <Link to="/home">Back To Home</Link>
+        <div className='bg-indigo-200 text-center shadow-lg w-3/4 md:w-2/6 rounded-xl p-10 mt-20 m-auto'>
+            <h3 className='md:text-2xl text-xl font-bold mb-4'>Page Not Found: ERROR 404</h3>
+            <p className='mb-2 font-bold'>Oops. The page you request is not match any route. Please try with correct URL.</p>
+            <button className='bg-blue-900 py-2 px-6 text-white rounded-lg font-bold text-center mx-auto'>
+                <Link to="/home">Back To Home</Link>
+             </button>
         </div>
     );
 };
